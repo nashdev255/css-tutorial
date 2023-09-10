d5ef309bea11544938fd48b4649da502f829466b
nashdev255 <yutori1107@gmail.com>
2023-09-11 03:11:59 +0900

    what is w-full

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,14 @@ export default function Home() {
           <div className='h-24 w-48 bg-lime-300 flex items-center justify-center'>bg-lime-300</div>
           <div className='h-24 w-48 bg-green-400 flex items-center justify-center'>bg-green-400</div>
         </div>
+        <div className='h-48 w-full bg-sky-400 flex items-center justify-center'>bg-sky-400</div>
       </div>
+      <footer className='h-24 w-full bg-purple-400 flex items-center'>
+        <div className='w-full mx-10 flex justify-between'>
+          <div>{ new Date().getFullYear() } &copy; Hello, World</div>
+          <div>Build with nashdev255</div>
+        </div>
+      </footer>
     </>
   )
 }
